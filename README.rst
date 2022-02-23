f8a024ec624f8d11d369d15fac8a36fb3c73ab48
Alexis <al.dacosta@f5.com>
2022-02-23 17:38:52 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,72 @@
+Customer flow
+##############################################################
+
+.. image:: ./_pictures/UFO_mamy.png
+   :align: center
+   :width: 800
+   :alt: All layers
+
+.. contents:: Contents
+    :local:
+
+Installation guide
+*****************************************
+
+- Install `Python <https://www.python.org/>`_
+- Install `PyCharm <https://www.jetbrains.com/pycharm/>`_ community edition
+- Open PyCharm
+    - Create a new project ``Get from VCS`` and copy paste `this github repository URI <https://github.com/nergalex/f5-bot-selenium.git>`_
+    - Attach a `Python interpreter <https://www.jetbrains.com/help/pycharm/configuring-python-interpreter.html>`_
+- Download `here <https://sites.google.com/chromium.org/driver/>`_ same Chrome driver as your Chrome browser ``chrome://settings/help``
+- OPTION: for other browser, follow `this guide <https://selenium-python.readthedocs.io/installation.html#installation>`_
+- Copy downloaded ``chromedriver(.exe)`` file in ``./_files/chromedriver.exe`` of your project
+- For MacOS only, allow ``chromedriver`` as described `here <https://stackoverflow.com/questions/60362018/macos-catalinav-10-15-3-error-chromedriver-cannot-be-opened-because-the-de>`_:
+
+.. code-block:: bash
+
+    xattr -d com.apple.quarantine chromedriver
+
+- Open file ``requirements.txt``
+- Click on  ``Install requirements``
+
+.. image:: ./_pictures/Install_requirements.png
+   :align: center
+   :width: 300
+   :alt: setUp
+
+- Choose **ONLY** those packages ton install: ``selenium``, ``requests``
+
+.. image:: ./_pictures/no_install_2captcha.png
+   :align: center
+   :width: 300
+   :alt: setUp
+
+Run
+*****************************************
+
+- In PyCharm, open ``play1.py``
+
+- Set global variables
+    - Note: CAPTCHA_API_KEY is 2CAPTCHA API key and **NOT** the the Google recaptcha site-key
+
+.. code-block:: bash
+
+        URI = "https://{{your_hackazon_uri}}"
+        LOGIN_USER = "test_user"
+        LOGIN_PASSWORD = "123456"
+        LOGIN_BIRTHDAY = "12/12/1981"
+        LOOP_ITERATION = 2
+
+- For Mac user, set local variable
+
+.. code-block:: bash
+
+        PATH = "./_files/chromedriver"
+
+- Go to the end of the file and click on the green triangle
+
+.. image:: ./_pictures/run_test.png
+   :align: center
+   :width: 300
+   :alt: setUp
+
---
+++ play1.py
@@ -0,0 +1,270 @@
+import pprint
+import time
+import unittest
+import random
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.touch_actions import TouchActions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+import json
+import requests
+import re
+
+
+class Flow1(unittest.TestCase):
+
+    def setUp(self):
+        global URI, LOGIN_USER, LOGIN_PASSWORD, LOGIN_BIRTHDAY, CAPTCHA_API_KEY, solver, USER_AGENT, LOOP_ITERATION
+        URI = "INPUT"
+        LOGIN_USER = "INPUT"
+        LOGIN_PASSWORD = "INPUT"
+        LOGIN_BIRTHDAY = "INPUT"
+        CAPTCHA_API_KEY = "INPUT"
+        LOOP_ITERATION = 2
+
+        # local
+        PATH = "./_files/chromedriver.exe"
+        USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-web-security")
+        options.add_argument("---user-data-dir=./_chromeTemp")
+        options.add_argument("--disable-features=CrossSiteDocumentBlockingIfIsolating")
+        options.add_argument("--disable-site-isolation-for-policy")
+        options.add_argument("–-allow-file-access-from-files")
+        options.add_argument("--auto-open-devtools-for-tabs")
+        options.add_argument("--show-taps")
+        options.add_experimental_option('w3c', False)
+
+        self.driver = webdriver.Chrome(
+            executable_path=PATH,
+            options=options
+        )
+        self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": USER_AGENT})
+        print("Run Selenium bot with User Agent: %s" % self.driver.execute_script("return navigator.userAgent;"))
+
+    def test_login(self):
+        self.session = requests.session()
+
+        # goto welcome page
+        self.driver.get(URI)
+        time.sleep(2)
+
+        # Accept data privacy
+        element = self.getElementId("popin_tc_privacy_button_2")
+        self.clickButton(element)
+
+        for i in range(LOOP_ITERATION):
+
+            # tous-les-jeux
+            tmp_uri = URI + "/tous-les-jeux?t=&o=new"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # tous-les-jeux-de-tirage
+            element = self.getElementId("tous-les-jeux-de-tirage")
+            self.clickButton(element)
+            time.sleep(0.25)
+
+            # tous-les-jeux-bingo-live-multijoueur
+            element = self.getElementId("tous-les-jeux-bingo-live-multijoueur")
+            self.clickButton(element)
+            time.sleep(0.25)
+
+            # euromillions-my-million
+            tmp_uri = URI + "/jeux-de-tirage/euromillions-my-million"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # loto
+            tmp_uri = URI + "/jeux-loto"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # illiko
+            tmp_uri = URI + "/jeux-illiko"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # bingo-live
+            tmp_uri = URI + "/bingo-live-et-multijoueur/bingo-live"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # bingo-live game
+            tmp_uri = URI + "/bingo-live-et-multijoueur/bingo-live/bingo-live-one"
+            self.driver.get(tmp_uri)
+            time.sleep(5)
+
+            # keno
+            tmp_uri = URI + "/jeux-keno"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # resultats
+            tmp_uri = URI + "/jeux-de-tirage/resultats"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # mag
+            tmp_uri = URI + "/mag"
+            self.driver.get(tmp_uri)
+            time.sleep(1)
+
+            # User account
+            element = self.getElementClass("icon-header-user")
+            self.clickButton(element)
+
+            # Login
+            element = self.getElementId("wsi-login-credentials-form-email-input-input")
+            self.setForm(element=element, input_data=LOGIN_USER)
+
+            # Password
+            element = self.getElementId("wsi-login-credentials-form-password-input-input")
+            self.setForm(element=element, input_data=LOGIN_PASSWORD)
+
+            # Birth date
+            element = self.getElementId("wsi-login-credentials-form-birthdate-input-input")
+            self.setForm(element=element, input_data=LOGIN_BIRTHDAY)
+
+            # Submit
+            element = self.getElementId("wsi-authenticate-button")
+            self.clickButton(element)
+            time.sleep(1)
+
+            # Skip
+            # element = self.getElementId("wsi-post-authentication-skip-optional-layers")
+            # if element is not None:
+            #     self.clickButton(element)
+            #     time.sleep(1)
+
+            # MyAccount
+            element = self.getElementClass("main-nav_connect-user_myaccount")
+            self.clickButton(element)
+            time.sleep(1)
+
+            # Retrait
+            element = self.getElementId("wsi-connected-layer-summary-wallets-withdrawal-button")
+            self.clickButton(element)
+            time.sleep(1)
+
+            # Completer
+            element = self.getElementClass("action-link-btFournirMapiece-origin")
+            self.clickButton(element)
+            time.sleep(1)
+
+            # User account
+            element = self.getElementClass("main-nav_connect-user_notif-icon")
+            self.clickButton(element)
+
+            # User account
+            element = self.getElementId("wsi-connected-layer-summary-disconnect-button")
+            self.clickButton(element)
+
+            time.sleep(1000)
+
+    def tearDown(self):
+        self.driver.close()
+
+    def clickButton(self, element):
+        interval_think = random.uniform(1.1, 1.5)
+        interval_get_mouse = random.uniform(1.1, 1.5)
+        interval_click = random.uniform(0.1, 0.3)
+        offset_x = random.uniform(1, 10)
+        offset_y = random.uniform(1, 10)
+
+        ActionChains(self.driver).pause(interval_think).\
+            move_by_offset(offset_x, offset_y).\
+            move_to_element(element).pause(interval_get_mouse).\
+            click_and_hold(element).pause(interval_click).release(element).\
+            perform()
+
+    def setForm(self, element, input_data):
+        interval_think = random.uniform(1.1, 1.5)
+        interval_get_mouse = random.uniform(1.1, 1.5)
+        interval_click = random.uniform(0.1, 0.3)
+        offset_x = random.uniform(50, 100) * -1
+        offset_y = random.uniform(50, 100) * -1
+
+        ActionChains(self.driver).pause(interval_think).\
+            move_by_offset(offset_x, offset_y).\
+            move_to_element(element).pause(interval_get_mouse).\
+            click_and_hold(element).pause(interval_click).release(element).\
+            perform()
+        element.clear()
+        string = []
+        string[:0] = input_data
+        for character in string:
+            element.send_keys(character)
+            interval = random.uniform(0.1, 0.35)
+            time.sleep(interval)
+
+    def getElementId(self, element_id):
+        try:
+            WebDriverWait(self.driver, 2).until(
+                expected_conditions.presence_of_element_located((By.ID, element_id))
+            )
+        except TimeoutException:
+            print("Oops!  Too long to retrieve element '%s'" % element_id)
+        try:
+            element = self.driver.find_element_by_id(element_id)
+        except NoSuchElementException:
+            print("Oops!  There is no valid element '%s'" % element_id)
+            return None
+        return element
+
+    def getElementTagName(self, tag_name):
+        try:
+            WebDriverWait(self.driver, 2).until(
+                expected_conditions.presence_of_element_located((By.TAG_NAME, tag_name))
+            )
+        except TimeoutException:
+            print("Oops!  Too long to retrieve element '%s'" % tag_name)
+        try:
+            elements = self.driver.find_elements_by_tag_name(tag_name)
+        except NoSuchElementException:
+            print("Oops!  There is no valid element '%s'" % tag_name)
+            return None
+        return elements
+
+    def getElementClass(self, element_class):
+        try:
+            WebDriverWait(self.driver, 2).until(
+                expected_conditions.presence_of_element_located((By.CLASS_NAME, element_class))
+            )
+        except TimeoutException:
+            print("Oops!  Too long to retrieve element '%s'" % element_class)
+        try:
+            element = self.driver.find_element_by_class_name(element_class)
+        except NoSuchElementException:
+            print("Oops!  There is no valid element '%s'" % element_class)
+        return element
+
+    def getElementName(self, element_name):
+        try:
+            WebDriverWait(self.driver, 2).until(
+                expected_conditions.presence_of_element_located((By.NAME, element_name))
+            )
+        except TimeoutException:
+            print("Oops!  Too long to retrieve element '%s'" % element_name)
+        try:
+            element = self.driver.find_element_by_name(element_name)
+        except NoSuchElementException:
+            print("Oops!  There is no valid element '%s'" % element_name)
+        return element
+
+
+if __name__ == "__main__":
+
+    unittest.main()
